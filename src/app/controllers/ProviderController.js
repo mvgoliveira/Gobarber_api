fa3b76dd9ad576b4342360963d004f56d1e86426
MarcusOliveira <Markusvi17@gmail.com>
2021-02-11 19:12:49 -0300

    Added lasted version of API

---
+++ src/app/controllers/ProviderController.js
@@ -0,0 +1,16 @@
+import User from '../models/User';
+import File from '../models/File';
+
+class ProviderController {
+   async index(req, res) {
+      const provider = await User.findAll({
+         where: { provider: true },
+         attributes: ['id', 'name', 'email'],
+         include: [{ model: File, as: 'avatar', attributes: [ 'id', 'name', 'path', 'url' ] }],
+      });
+
+      return res.json(provider);
+   }
+}
+
+export default new ProviderController();
---
+++ src/database/migrations/20210121003530-create-appointments.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('appointments', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      user_id: {
+        type: Sequelize.INTEGER, 
+        references: { model: 'users', keys: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true
+      },
+
+      provider_id: {
+        type: Sequelize.INTEGER, 
+        references: { model: 'users', keys: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true
+      },
+
+      canceled_at: {
+        type: Sequelize.DATE,
+      },
+      
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+      
+    });
+  },
+
+  down: async queryInterface => {
+    await queryInterface.dropTable('files');
+  }
+};
